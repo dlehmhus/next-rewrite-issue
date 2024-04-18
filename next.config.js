09414c3505af9e09411cb1a361aa728ed6e75a8a
Dario Lehmhus <dario.lehmhus@ffw.com>
2024-04-18 09:24:16 +0200

    add issue example

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return {
+      afterFiles: [
+        {
+          source: "/asset-prefix/:path*",
+          destination: "/:path*",
+        },
+        {
+          source: "/not-asset-prefix/:path*",
+          destination: "/:path*",
+        },
+      ],
+    };
+  },
+  assetPrefix: "/asset-prefix",
 };
 
 module.exports = nextConfig;
